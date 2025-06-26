91b5a6c116052f5c2afb3936004699ead291f2ed
Zaiqiang Dong <dongzq@autoai.com>
2025-06-26 14:57:07 +0800

    modify hyprwayland-scanner

--- hyprwayland-scanner.sh
+++ hyprwayland-scanner.sh
@@ -2,9 +2,10 @@
 
 sudo apt install libpugixml-dev
 
-tag=0.4.4
-git clone --recursive -b $tag https://github.com/hyprwm/hyprlang.git
+tag=v0.4.4
+git clone https://github.com/hyprwm/hyprlang.git
 cd hyprwayland-scanner
+git checkout $tag
 cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
 cmake --build build -j `nproc`
 sudo cmake --install build
