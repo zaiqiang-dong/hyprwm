fda00d3907f188c354f104c605d4fac2b246b2e2
Zaiqiang Dong <dongzq@autoai.com>
2025-07-03 09:22:43 +0800

    add config

--- scripts/worksapce.sh
+++ scripts/worksapce.sh
@@ -1,22 +1,50 @@
 #!/bin/bash
 
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIG_FILE=$SCRIPT_DIR/../config/hypr/workspace.conf
 
-MONITOR_NAME=$(hyprctl monitors | grep "ID 0"  | cut -d " " -f 2)
-# 写入临时配置文件
-echo "workspace = 1, monitor:$MONITOR_NAME" >  $CONFIG_FILE
-echo "workspace = 2, monitor:$MONITOR_NAME" >> $CONFIG_FILE
-echo "workspace = 3, monitor:$MONITOR_NAME" >> $CONFIG_FILE
-echo "workspace = 4, monitor:$MONITOR_NAME" >> $CONFIG_FILE
-echo "workspace = 5, monitor:$MONITOR_NAME" >> $CONFIG_FILE
+monitor_master=""
+monitor_sub=""
+monitor_count=0
 
+for dev in /sys/class/drm/*/status; do
+    if [[ $(cat $dev) == "connected" ]]; then
+        name=$(basename $(dirname $dev) | cut -d'-' -f2-)
+        let monitor_count=monitor_count+1
+        if [[ monitor_count -eq 1 ]]; then
+            monitor_master=$name
+        else
+            monitor_sub=$name
+        fi
+    fi
+done
+# echo $monitor_count
+# echo $monitor_master
+# echo $monitor_sub
 
-MONITOR_NAME=$(hyprctl monitors | grep "ID 1"  | cut -d " " -f 2)
-echo "workspace = 6, monitor:$MONITOR_NAME" >>  $CONFIG_FILE
-echo "workspace = 7, monitor:$MONITOR_NAME" >>  $CONFIG_FILE
-echo "workspace = 8, monitor:$MONITOR_NAME" >>  $CONFIG_FILE
-echo "workspace = 9, monitor:$MONITOR_NAME" >>  $CONFIG_FILE
-echo "workspace = 10, monitor:$MONITOR_NAME" >> $CONFIG_FILE
-echo "workspace = 11, monitor:$MONITOR_NAME" >> $CONFIG_FILE
+if [[ $monitor_count -eq 1 ]]; then
+    echo "workspace = 1,  monitor:$monitor_master" >$CONFIG_FILE
+    echo "workspace = 2,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 3,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 4,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 5,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 6,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 7,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 8,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 9,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 10, monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 11, monitor:$monitor_master" >>$CONFIG_FILE
+else
+    echo "workspace = 1,  monitor:$monitor_master" >$CONFIG_FILE
+    echo "workspace = 2,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 3,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 4,  monitor:$monitor_master" >>$CONFIG_FILE
+    echo "workspace = 5,  monitor:$monitor_master" >>$CONFIG_FILE
+
+    echo "workspace = 6,  monitor:$monitor_sub" >>$CONFIG_FILE
+    echo "workspace = 7,  monitor:$monitor_sub" >>$CONFIG_FILE
+    echo "workspace = 8,  monitor:$monitor_sub" >>$CONFIG_FILE
+    echo "workspace = 9,  monitor:$monitor_sub" >>$CONFIG_FILE
+    echo "workspace = 10, monitor:$monitor_sub" >>$CONFIG_FILE
+    echo "workspace = 11, monitor:$monitor_sub" >>$CONFIG_FILE
+fi
