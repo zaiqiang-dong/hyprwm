8e45d1e35d99c7e19b363216232c1f7959db3a52
Zaiqiang Dong <dongzq@autoai.com>
2025-08-13 09:37:45 +0800

    add config

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+wget -qO- https://git.io/papirus-icon-theme-install | sh
 gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
-gsettings set org.gnome.desktop.interface icon-theme "Yaru-dark"
+# gsettings set org.gnome.desktop.interface icon-theme "Yaru-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
