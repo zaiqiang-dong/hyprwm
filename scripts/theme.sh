09427bd8fc9eb25eda2b9ee1cebea6ca4c8020d5
Zaiqiang Dong <dongzq@autoai.com>
2026-01-03 22:07:30 +0800

    add config

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.icons" sh
+# wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.icons" sh
 sudo snap install icon-theme-papirus
+sudo apt install papirus-icon-theme
 gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
 gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
