cb3eb726ce2ad19b2cd3fbdd09fbe7831ca86ca4
Zaiqiang Dong <dongzq@autoai.com>
2026-01-05 22:29:56 +0800

    add config

--- back-file.sh
+++ back-file.sh
@@ -11,3 +11,6 @@ git checkout ./config/hypr/workspace.conf
 cp -r ~/.config/waybar ./config/
 cp -r ~/.config/rofi ./config/
 cp -r ~/.config/swaync ./config/
+cp -r ~/.config/gtk-3.0 ./config/
+cp -r ~/.config/gtk-4.0 ./config/
+cp -r ~/.gtkrc-2.0 ./config/gtk-2.0/gtkrc-2.0
