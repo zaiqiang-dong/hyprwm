17c10e47b66fdf512daa71c75ae49bb95745f0a7
Zaiqiang Dong <dongzq@autoai.com>
2026-01-05 14:25:04 +0800

    add config

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -3,7 +3,11 @@
 # wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.icons" sh
 sudo snap install icon-theme-papirus
 sudo apt install papirus-icon-theme
-sudo mv /usr/share/icons/gnome /usr/share/icons/back-gnome
-sudo mv /usr/share/icons/Papirus-Dark /usr/share/icons/gnome
-# gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
-# gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+sudo mv /usr/share/icons/gnome /usr/share/icons/gnome-back
+sudo mv /usr/share/icons/Yaru /usr/share/icons/Yaru-back
+sudo mv /usr/share/icons/Yaru-dark /usr/share/icons/Yaru-dark-back
+sudo cp /usr/share/icons/Papirus /usr/share/icons/gnome -r
+sudo cp /usr/share/icons/Papirus /usr/share/icons/Yaru -r
+sudo cp /usr/share/icons/Papirus /usr/share/icons/Yaru-dark -r
+gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
