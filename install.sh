b97f69c3ded7974a6d6e6b493f1627e4d7342e92
Zaiqiang Dong <dongzq@autoai.com>
2026-01-06 09:21:25 +0800

    add config

--- install.sh
+++ install.sh
@@ -119,6 +119,8 @@ sudo apt-get install -y lm-sensors
 sudo apt-get install -y cliphist
 sudo apt-get install -y copyq
 sudo apt-get install -y libqt5printsupport5t64
+sudo apt-get install -y gtk2-engines-murrine
+sudo apt-get install -y gtk2-engines-pixbuf
 # sudo apt-get install -y xdg-desktop-portal-kde
 
 ./scripts/waybar.sh
