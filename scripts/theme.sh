d7ec4c7d80f83b2a327f0f74c113e946503a133b
Zaiqiang Dong <dongzq@autoai.com>
2026-01-07 23:39:46 +0800

    add config

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR ""$HOME/.icons" sh
+# wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.icons" sh
 sudo snap install icon-theme-papirus
 sudo apt install papirus-icon-theme
 
