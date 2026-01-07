b7c70061ac97164b117f8437b29121c11ad8b1c1
Zaiqiang Dong <dongzq@autoai.com>
2026-01-07 23:46:15 +0800

    add config

--- deploy.sh
+++ deploy.sh
@@ -34,3 +34,18 @@ fi
 ln -s ~/.local/share/themes/Catppuccin-Green-Dark/gtk-4.0 ~/.config/gtk-4.0
 
 git checkout .
+
+gsettings set org.gnome.desktop.interface gtk-theme "Catppuccin-Green-Dark"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+gsettings set org.gnome.desktop.interface font-name "JetBrainsMono Nerd Font Ultra-Bold 12"
+gsettings set org.gnome.desktop.interface cursor-theme "Adwaita"
+gsettings set org.gnome.desktop.interface cursor-size "24"
+gsettings set org.gnome.desktop.interface toolbar-style "both-horiz"
+gsettings set org.gnome.desktop.interface toolbar-icons-size "large"
+gsettings set org.gnome.desktop.interface font-hinting "slight"
+gsettings set org.gnome.desktop.interface font-antialiasing "grayscale"
+gsettings set org.gnome.desktop.interface font-rgba-order "rgb"
+gsettings set org.gnome.desktop.interface text-scaling-factor "1.0"
+gsettings set org.gnome.desktop.interface color-scheme "default"
+gsettings set org.gnome.desktop.interface event-sounds "true"
+gsettings set org.gnome.desktop.interface input-feedback-sounds "true"
--- scripts/theme.sh
+++ scripts/theme.sh
@@ -4,17 +4,3 @@
 sudo snap install icon-theme-papirus
 sudo apt install papirus-icon-theme
 
-gsettings set org.gnome.desktop.interface gtk-theme "Catppuccin-Green-Dark"
-gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
-gsettings set org.gnome.desktop.interface font-name "JetBrainsMono Nerd Font Ultra-Bold 12"
-gsettings set org.gnome.desktop.interface cursor-theme "Adwaita"
-gsettings set org.gnome.desktop.interface cursor-size "24"
-gsettings set org.gnome.desktop.interface toolbar-style "both-horiz"
-gsettings set org.gnome.desktop.interface toolbar-icons-size "large"
-gsettings set org.gnome.desktop.interface font-hinting "slight"
-gsettings set org.gnome.desktop.interface font-antialiasing "grayscale"
-gsettings set org.gnome.desktop.interface font-rgba-order "rgb"
-gsettings set org.gnome.desktop.interface text-scaling-factor "1.0"
-gsettings set org.gnome.desktop.interface color-scheme "default"
-gsettings set org.gnome.desktop.interface event-sounds "true"
-gsettings set org.gnome.desktop.interface input-feedback-sounds "true"
