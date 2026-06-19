8bac55326198bdce87cceb4f6881e4a556d6ac24
Zaiqiang Dong <dongzq@autoai.com>
2026-06-19 15:19:58 +0800

    add config

--- scripts/waybar.sh
+++ scripts/waybar.sh
@@ -3,8 +3,11 @@
 git clone https://github.com/Alexays/Waybar
 cd Waybar
 
-meson setup build
-# meson setup build -Dtests=disabled
+if [[ -f /usr/local/bin/waybar ]]; then
+    meson setup build -Dtests=disabled
+else
+    meson setup build
+fi
 ninja -C build
 sudo ninja -C build install
 if [ $? -ne 0 ]; then
